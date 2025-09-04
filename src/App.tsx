e8b15e179ec37a5a951b548117a606140ea6990a
yakirodmorfera@gmail.com <yakirodmorfera@gmail.com>
2025-09-04 13:27:06 -0300

    Mudando estilos

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,9 @@ import './styles/global.css';
 import './styles/theme.css';
 
 export function App() {
-  console.log('Olá, mundo!');
   return (
     <>
-      <Heading>Olá Mundo!</Heading>
-      <Heading>Olá Mundo! 2</Heading>
-      <Heading>Olá Mundo! 3</Heading>
+      <Heading attr='String' sttr2={123} >Olá Mundo!</Heading>
       <p>
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maiores
         delectus quaerat aperiam rem voluptas porro quia voluptatum
--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,5 +1,6 @@
 import styles from './Heading.module.css';
 
 export function Heading(props) {
+    console.log(props)
     return <h1 className={styles.heading}>{props.children}</h1>;
 }
